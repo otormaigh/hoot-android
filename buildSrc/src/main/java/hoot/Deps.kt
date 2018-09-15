bb9b2996f44fe931584c7fadbce20524e98e7880
Elliot Tormey <elliot.tormey@gmail.com>
2018-09-15 19:22:12 +0100

    initial commit

---
+++ buildSrc/src/main/java/hoot/Deps.kt
@@ -0,0 +1,12 @@
+package hoot
+
+object Deps {
+  // org.jetbrains.kotlin
+  const val kotlin = "1.2.70"
+
+  // com.google.dagger
+  const val dagger = "2.17"
+
+  // androidx.room
+  const val arch_room = "2.0.0-rc01"
+}
